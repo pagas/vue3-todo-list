10f24cc1e811abcd13826bc22a62e2ca57934d38
Paulius G <paulius@2simple.com>
2024-03-22 22:06:57 +0000

    Refactor TodoList component to use local storage for item persistence

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,36 +1,61 @@
 <script lang="ts" setup>
 import ListItem from './ListItem.vue'
-import { ref, computed } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import type { Ref } from 'vue'
 
 type Item = {
     title: string,
     checked?: boolean
 }
-const listItems: Ref<Item[]> = ref([
-    { title: 'Make a todo list app', checked: true },
-    { title: 'Predict the weather', checked: false },
-    { title: 'Play some tunes', checked: false },
-    { title: 'Let\'s get cooking', checked: false },
-    { title: 'Pump some iron', checked: false },
-    { title: 'Track my expenses', checked: false },
-    { title: 'Organize a game night', checked: false },
-    { title: 'Learn a new language', checked: false },
-    { title: 'Publish my work', checked: true }
-]);
+const storageItems: Ref<Item[]> = ref([]);
+
+onMounted(() => {
+    storageItems.value = getFromStorage();
+    initListItems();
+})
 
 const sortedList = computed(() =>
-    [...listItems.value].sort((a, b) => (a.checked ? 1 : 0) - (b.checked ? 1 : 0))
+    [...storageItems.value].sort((a, b) => (a.checked ? 1 : 0) - (b.checked ? 1 : 0))
 )
+
+const initListItems = (): void => {
+    if (storageItems.value?.length > 0) return;
+
+    const listItems = [
+        { title: 'Make a todo list app', checked: true },
+        { title: 'Predict the weather', checked: false },
+        { title: 'Read some comics', checked: false },
+        { title: 'Let\'s get cooking', checked: false },
+        { title: 'Pump some iron', checked: false },
+        { title: 'Track my expenses', checked: false },
+        { title: 'Organise a game night', checked: false },
+        { title: 'Learn a new language', checked: false },
+        { title: 'Publish my work' }
+    ]
+    setToStorage(listItems)
+    storageItems.value = listItems
+}
+
+const setToStorage = (items: Item[]): void => {
+    localStorage.setItem('list-items', JSON.stringify(items))
+}
+const getFromStorage = (): Item[] | [] => {
+    const stored = localStorage.getItem('list-items')
+    if (stored) {
+        return JSON.parse(stored)
+    }
+    return []
+}
 
 const updateItem = (item: Item): void => {
     const updatedItem = findItemInList(item)
     if (!updatedItem) return;
 
     toggleItemChecked(updatedItem)
+    setToStorage(storageItems.value);
 }
 const findItemInList = (item: Item): Item | undefined => {
-    return listItems.value.find(
+    return storageItems.value.find(
         (itemInList: Item) => itemInList.title === item.title
     )
 }
